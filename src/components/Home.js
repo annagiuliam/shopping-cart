3261b602b395f78efe67343866d50c533790ab0c
annagiuliam <annagiulia.musso@gmail.com>
2021-01-12 18:13:56 +0100

    core component styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
 const Home = () => {
+    
     return (
-        <div className="home-container">
+        <div className="home-container" >
             <h1>The little cake shop</h1>
             <h2>Design cakes for any occasion</h2>
             <Link to='/shop'>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -6,16 +6,19 @@ import CakeCard from './CakeCard'
 function Shop() {
   console.log(cakes)
   return (
-    <div className="App">
-     {cakes.map(cake => {
-       return (
-         <CakeCard  
-         src={cake.src}
-         name={cake.name}
-         price={cake.price}
-         />
-       )
-     })}
+    <div className="cakes-container">      
+      
+        {cakes.map(cake => {
+          return (
+            <CakeCard  
+            src={cake.src}
+            name={cake.name}
+            price={cake.price}
+            key={cake.id}
+            />
+          )
+        })}
+    
     </div>
   );
 }
